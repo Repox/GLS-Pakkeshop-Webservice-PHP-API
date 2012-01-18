183bae6d90c2d0fb4deabd387d7bb3e62c5ea467
Repox <storm@err0r.dk>
2012-01-18 13:49:37 +0100

    Oprettet eksempel til at finde pakkeshops ud fra postnummer

---
+++ eksempler/zipcode/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>GLS Pakkeshop SOAP Service eksempel: basic</title>
+		<style type="text/css">
+		body { font-family: Calibri, Verdana, "Sans Serif"; }
+		label { display: block; }			
+		</style>
+	</head>
+	<body>
+
+	<h2>Et basalt eksempel på brug af klassen</h2>
+	
+	<p>
+		Dette eksempel viser hvordan du kan søge efter GLS Pakkeshops ud fra postnummer ved hjælp af en simpel form.
+	</p>
+	
+	<form method="post" action="search.php">		
+		<p>
+			<label for="zipcode">Postnummer:</label>
+			<input type="text" name="zipcode" id="zipcode">
+		</p>
+		
+		<p>
+			<input type="submit" name="search" value="Søg">
+		</p>
+	</form>
+		
+	</body>
+</html>
